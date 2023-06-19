a2c6404b662de0aef46d99a70a602619f3bc43a3
Logan Gray <loganallengray@gmail.com>
2023-06-19 17:05:36 -0500

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'reactstrap';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Placeholder
+      </header>
+    </div>
+  );
+}
+
+export default App;
