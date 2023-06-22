21859675c5d3098b3844dd5df2b6d6c5c2de87dd
Logan Gray <loganallengray@gmail.com>
2023-06-22 16:57:34 -0500

    installed react router dom incorrectly, recorrected

--- client/src/components/ApplicationView.tsx
+++ client/src/components/ApplicationView.tsx
@@ -1,10 +1,14 @@
 // Allows access to every view.
 
 import { Routes, Route, Navigate } from "react-router-dom";
+import Homepage from "./Homepage";
 
 const ApplicationView = () => {
     return (
         <Routes>
+            <Route path="/">
+                <Route index element={<Homepage />} />
+            </Route>
         </Routes>
     )
 }
