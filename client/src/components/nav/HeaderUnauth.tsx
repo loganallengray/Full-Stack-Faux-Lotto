7026c8a968cc185399c83f916100e4275b69b047
Logan Gray <loganallengray@gmail.com>
2023-06-22 18:02:51 -0500

    header set up

--- client/src/components/nav/HeaderUnauth.tsx
+++ client/src/components/nav/HeaderUnauth.tsx
@@ -2,10 +2,17 @@
 // Login and Register links at the top right.
 // Homepage, Gamelist, Login, and Register are all available.
 
+import { Link } from "react-router-dom";
+import { Nav, NavItem } from "reactstrap";
+
 const HeaderUnauth = () => {
     return (
-        <>
-        </>
+        <Nav>
+            <NavItem><Link to="">Faux Lotto</Link></NavItem>
+            <NavItem><Link to="games">Games</Link></NavItem>
+            <NavItem><Link to="login">Login</Link></NavItem>
+            <NavItem><Link to="register">Register</Link></NavItem>
+        </Nav>
     )
 }
 
